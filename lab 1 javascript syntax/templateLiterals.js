cb524cfa6c222fbe1919a1161e264305e59da936
Rafid Hassan Risun <87337202+RafidRisun@users.noreply.github.com>
2024-02-09 18:40:18 +0600

    template literal javascript

---
+++ lab 1 javascript syntax/templateLiterals.js
@@ -0,0 +1,13 @@
+const name = "Rafid";
+const greeting = `Hello ${name}!`;
+console.log(greeting);
+
+const multiline = `This is 
+a multiline
+string.`;
+console.log(multiline);
+
+const a = 5;
+const b = 10;
+const sum = `The sum of ${a} and ${b} is ${a+b}.`;
+console.log(sum);
