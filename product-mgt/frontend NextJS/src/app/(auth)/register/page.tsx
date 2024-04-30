788873cb59aed24ca11131a100719bca53fc0a22
Rafid Hassan Risun <87337202+RafidRisun@users.noreply.github.com>
2024-04-30 12:37:57 +0600

    Add files via upload

--- product-mgt/frontend NextJS/src/app/(auth)/register/page.tsx
+++ product-mgt/frontend NextJS/src/app/(auth)/register/page.tsx
@@ -3,23 +3,21 @@ import { useRouter } from "next/router"
 export default function Register() {
   const router = useRouter();
 
-  const user = () => {
-    router.push("/user");
-  }
-
-  const reg = () => {
-    router.push("/register");
+  const login = () => {
+    router.push("/login/user");
   }
 
   return (
     <div>
-        <h1>User Login Page</h1><br/>
+        <h1>User Registration Page</h1><br/>
         <h3>User Name:</h3>
         <input type="text" id="username"></input><br/>
         <h3>Password</h3>
         <input type="password" id="password"></input><br/>
-        <input type="button" onClick={user}>Login</input><br/>
-        <input type="register" onClick={reg}>Register</input>
+        <h3>Confirm Password</h3>
+        <input type="password" id="password"></input><br/>
+        <input type="button" onClick={login}>Register</input><br/>
+        <input type="register" onClick={login}>Login</input>
     </div>
   )
 }
