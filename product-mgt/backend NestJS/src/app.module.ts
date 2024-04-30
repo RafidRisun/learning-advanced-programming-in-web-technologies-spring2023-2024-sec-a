5d7fc1863b63086caad59c4b7bb58954e0728e01
Rafid Hassan Risun <87337202+RafidRisun@users.noreply.github.com>
2024-04-30 12:53:28 +0600

    Add files via upload

---
+++ product-mgt/backend NestJS/src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { AdminModule } from './admin/admin.module';
+import { ProductModule } from './product/product.module';
+
+@Module({
+  imports: [UserModule, AdminModule, ProductModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
