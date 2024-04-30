2e334fa2e79ba182d378261537ba3bace00b75e3
Rafid Hassan Risun <87337202+RafidRisun@users.noreply.github.com>
2024-04-30 23:09:14 +0600

    Add files via upload

--- product-mgt/backend NestJS/src/admin/authAdmin/authAdmin.controller.ts
+++ product-mgt/backend NestJS/src/admin/authAdmin/authAdmin.controller.ts
@@ -1,9 +1,21 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { AuthAdminService } from './authAdmin.service';
-
-@Controller('admin')
+import { CreateAdminDto, loginDTO } from '../dto/create-admin.dto';
+import * as bcrypt from 'bcrypt';
+@Controller('admin/auth')
 export class AuthAdminController {
-  constructor(private readonly authService: AuthAdminService) {}
+  constructor(private authService: AuthAdminService) { }
 
-  
+  @Post('register')
+  @UsePipes(new ValidationPipe())
+  async addUser(@Body() myobj: CreateAdminDto,): Promise<Object> {
+    const salt = await bcrypt.genSalt();
+    const hashedpassword = await bcrypt.hash(myobj.password, salt);
+    myobj.password = hashedpassword;
+    return this.authService.signUp(myobj);
+  }
+  @Post('login')
+  signIn(@Body() logindata: loginDTO) {
+    return this.authService.signIn(logindata);
+  }
 }
--- product-mgt/backend NestJS/src/user/auth/auth.controller.ts
+++ product-mgt/backend NestJS/src/user/auth/auth.controller.ts
@@ -1,9 +1,21 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
-
-@Controller('user')
+import { CreateUserDto, loginDTO } from '../dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
+@Controller('user/auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private authService: AuthService) { }
 
-  
+  @Post('register')
+  @UsePipes(new ValidationPipe())
+  async addUser(@Body() myobj: CreateUserDto,): Promise<Object> {
+    const salt = await bcrypt.genSalt();
+    const hashedpassword = await bcrypt.hash(myobj.password, salt);
+    myobj.password = hashedpassword;
+    return this.authService.signUp(myobj);
+  }
+  @Post('login')
+  signIn(@Body() logindata: loginDTO) {
+    return this.authService.signIn(logindata);
+  }
 }
