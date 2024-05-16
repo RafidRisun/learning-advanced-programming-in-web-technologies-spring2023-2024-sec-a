6405879e8ff20b5267bef441aee74b22f62f4589
Rafid Hassan Risun <87337202+RafidRisun@users.noreply.github.com>
2024-05-16 17:33:13 +0600

    frontend

---
+++ finalLabExamCRUD/frontend/src/app/employeelogin/page.tsx
@@ -0,0 +1,23 @@
+export default function EmployerLogin() {
+    const handleSubmit = () => {
+        
+    }
+    return ( 
+      <html>
+        <form onSubmit={handleSubmit}>
+        <table>
+            <tr>
+                <td>
+                    Employer Name: 
+                </td>
+                <td>
+                    <input type="text"></input>
+                </td>
+            </tr>
+        </table>  
+        </form>
+        
+      </html>
+     );
+  }
+  
---
+++ finalLabExamCRUD/frontend/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return ( 
+    <html>
+      <a href="/adminlogin">Admin</a>
+      <a href="/employeelogin">Employee</a>
+    </html>
+   );
+}
