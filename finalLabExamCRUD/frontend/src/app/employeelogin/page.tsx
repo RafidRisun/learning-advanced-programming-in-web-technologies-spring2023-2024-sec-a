7c2c099dfb33aa626da930883638a130d7f0b957
Rafid Hassan Risun <87337202+RafidRisun@users.noreply.github.com>
2024-05-16 17:46:45 +0600

    Add files via upload

--- finalLabExamCRUD/frontend/src/app/employeelogin/page.tsx
+++ finalLabExamCRUD/frontend/src/app/employeelogin/page.tsx
@@ -1,7 +1,27 @@
+import React, { useState } from "react";
+interface FormData{
+    employerName: string;
+    password: string;
+}
+
 export default function EmployerLogin() {
-    const handleSubmit = () => {
-        
-    }
+    const [formData, setFormData] = useState<FormData>({employerName: '', password: '' });
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        try {
+            const formDataObject = new FormData();
+            formDataObject.append('employerName', formData.employerName);
+            formDataObject.append('description', formData.password);
+            const response = await axios.post(`http://localhost:3000/employee/login`, formDataObject, {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+        } catch (error) {
+            toast.error('Failed to send :(');
+        }
+    };
     return ( 
       <html>
         <form onSubmit={handleSubmit}>
@@ -11,7 +31,15 @@ export default function EmployerLogin() {
                     Employer Name: 
                 </td>
                 <td>
-                    <input type="text"></input>
+                    <input type="text" id="employerName" name="employerName" onChange={(e) => setFormData(prevState => ({ ...prevState, employerName: e.target.value }))}/>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Password: 
+                </td>
+                <td>
+                    <input type="text" id="password" name="password" onChange={(e) => setFormData(prevState => ({ ...prevState, password: e.target.value }))}/>
                 </td>
             </tr>
         </table>  
